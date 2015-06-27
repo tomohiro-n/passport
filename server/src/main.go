0606931aa004f21589cfe8422f9af3d3724532f4
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-06-28 00:08:20 +0900

    initial commit for destination api

--- server/src/main.go
+++ server/src/main.go
@@ -4,36 +4,61 @@ import (
 	"./server"
 	"net/http"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"fmt"
 	"encoding/json"
 )
 
-type queryRequestStruct struct {
+type QueryRequestStruct struct {
 	TwitterId string
 	Query string
 }
 
-type queryResponseStruct struct {
-	TwitterIds []string `json:"twitterids"`
+type QueryResponseStruct struct {
+	Items []QueryResponseElement `json:"items"`
+}
+
+type QueryResponseElement struct {
+	TwitterId string `json:"twitterid"`
+	Url string `json:"url"`
+}
+
+type DestRequestStruct struct {
+	TwitterId string
+	Query string
+	Url string
 }
 
 var queries *mgo.Collection
 
 func queryHandler(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	var body queryRequestStruct
+	var body QueryRequestStruct
+	err := decoder.Decode(&body)
+	if err != nil {
+		panic(err)
+	}
+	queryString := body.Query
+	fmt.Println("query received: " + queryString)
+	var founds []QueryResponseElement
+	queries.Find(bson.M{"query": body.Query}).All(&founds)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	result := QueryResponseStruct{founds}
+	json.NewEncoder(w).Encode(result)
+
+	queries.Insert(body)
+}
+
+func destHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var body DestRequestStruct
 	err := decoder.Decode(&body)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(body)
-	queryString := body.Query
-	fmt.Println("query received: " + queryString)
-	queries.Insert(body)
-
-	dummyResponse := queryResponseStruct{[]string{"a", "b", "c", "d"}}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	json.NewEncoder(w).Encode(dummyResponse)
+	queries.Update(bson.M{"twitterid": body.TwitterId, "query": body.Query},
+		body)
 }
 
 func main() {
@@ -41,6 +66,7 @@ func main() {
 	server.ConnectDatabase()
 	queries = server.Database.C(server.QUERIES_COLLECTION)
 	http.HandleFunc("/query", queryHandler)
+	http.HandleFunc("/dest", destHandler)
 	//http.ListenAndServe(":8080", nil)
 	err := http.ListenAndServeTLS(":9444", "./public_key", "private_key", nil)
 	if err != nil {
