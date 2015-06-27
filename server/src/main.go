142bbfa223e89dbc93548c375c7c210cf64df139
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-06-27 18:39:21 +0900

    dummy response added

--- server/src/main.go
+++ server/src/main.go
@@ -8,15 +8,20 @@ import (
 	"encoding/json"
 )
 
-type queryStruct struct {
+type queryRequestStruct struct {
+	TwitterId string
 	Query string
+}
+
+type queryResponseStruct struct {
+	TwitterIds []string `json:"twitterids"`
 }
 
 var queries *mgo.Collection
 
 func queryHandler(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	var body queryStruct
+	var body queryRequestStruct
 	err := decoder.Decode(&body)
 	if err != nil {
 		panic(err)
@@ -24,8 +29,10 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(body)
 	queryString := body.Query
 	fmt.Println("query received: " + queryString)
-
 	queries.Insert(body)
+
+	dummyResponse := queryResponseStruct{[]string{"a", "b", "c", "d"}}
+	json.NewEncoder(w).Encode(dummyResponse)
 }
 
 func main() {
