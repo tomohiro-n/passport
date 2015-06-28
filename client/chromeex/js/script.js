b6794ef5c4f1d113f7d07ba634fe890223195aed
kotamat <kota1861@gmail.com>
2015-06-28 11:07:01 +0900

    change user

--- client/chromeex/js/script.js
+++ client/chromeex/js/script.js
@@ -1,54 +1,86 @@
 OAuth.initialize('LpvB_HDMk5DoD1biSEDhrrDwD00');
+
 $(function(){
 	$('body').after('<div id="passport-container"></div>');
 	$('#passport-container').on('click', '#passport-container_moreButton', function(){
 		$('#passport-container').addClass('passport-container_collapse');
 	});
 });
+
 var onQuery = function(){
 	var $form = $('#lst-ib');
 	var $appendedHtml;
 	var twitteridKey = 'user_id';
+	var queryObject;
+	var nowUserIndex = 0;
+
 
 	$.get(chrome.extension.getURL('oauth.html'), function(data){
 
-		function appendUser(twitter_user_id){
+		$('#passport-container').on('click', '#passport-container_changeUserButton', function(){
+			if(queryObject.items[nowUserIndex + 1] == undefined){
+				nowUserIndex = 0;
+			} else {
+				nowUserIndex ++;
+			}
+			appendContent();
+		});
+
+		function appendUser(){
+			OAuth.popup('twitter', {cache: true}).then(function(result) {
+				return result.get('/1.1/users/show.json?screen_name=' + queryObject.items[nowUserIndex].twitterid);
+			}).then(function(data){
+				var name = data.screen_name;
+				var img = data.profile_image_url;
+				var url = data.url || 'https://twitter.com/' + name;
+				$('a#passport-container_twitterIcon').attr('href',url).html('<img src="' + img + '" alt="" />');
+				$('.passport-container_twitterName').html('<a href="' + url + '" target="_blank">' + name + 'さん</a>');
+			});
+		}
+
+		function appendOtherQueries(){
+			if(queryObject.items[nowUserIndex].otherqueries == null){
+				return;
+			}
+			$.get(chrome.extension.getURL('more.html'), function(data){
+				var $moreHtml = $(data);
+				for(var i in queryObject.items[nowUserIndex].otherqueries.length){
+					$moreHtml.find('.passport-container_more').text('さらに' + i + 'で検索しました');
+					$moreHtml.find('.passport-container_clickedHistoryName').html('<a href="' + i + '">' + i + '</a>');
+					$('#passport-container').append($moreHtml.html());
+				}
+			});
+		}
+
+		function appendContent(){
+			// zero
+			if(queryObject.items == null){
+				$('#passport-container').html('');
+				return;
+			}
+			// first
+			appendUser(queryObject, nowUserIndex);
+
+			$('.passport-container_clickedSite_name').html('<a href="' + queryObject.items[nowUserIndex].url + '">' + queryObject.items[nowUserIndex].title + '</a>');
+
+			// other
+			appendOtherQueries(queryObject, nowUserIndex);
+
+			if(queryObject.items.length == 1){
+				return;
+			}
+		}
+
+		function setContent(twitter_user_id){
 			$.ajax({
 				type: "POST",
 				url: "//localhost:9444/query",
 				data: JSON.stringify({'query': $form.val(), 'twitterid': twitter_user_id}),
 				dataType: 'json',
 				success: function(object){
-					// zero
-					if(object.items == null){
-						$('#passport-container').html('');
-						return;
-					}
-					// first
-					OAuth.popup('twitter', {cache: true}).then(function(result) {
-						return result.get('/1.1/users/show.json?screen_name=' + object.items[0].twitterid);
-					}).then(function(data){
-						var name = data.screen_name;
-						var img = data.profile_image_url;
-						var url = data.url || 'https://twitter.com/' + name;
-						$('a#passport-container_twitterIcon').attr('href',url).html('<img src="' + img + '" alt="" />');
-						$('.passport-container_twitterName').html('<a href="' + url + '" target="_blank">' + name + 'さん</a>');
-					});
+					queryObject = object;
 
-					$('.passport-container_clickedSite_name').html('<a href="' + object.items[0].url + '">' + object.items[0].title + '</a>');
-
-					if(object.items.length == 1){
-						return;
-					}
-					// other
-					$.get(chrome.extension.getURL('more.html'), function(data){
-						var $moreHtml = $(data);
-						for(var i = 1; i < object.items.length; i++){
-							$moreHtml.find('.passport-container_more').text('さらに' + i + 'で検索しました');
-							$moreHtml.find('.passport-container_clickedHistoryName').html('<a href="' + i + '">' + i + '</a>');
-							$('#passport-container').append($moreHtml.html());
-						}
-					});
+					appendContent();
 				}
 			});
 		}
@@ -66,12 +98,12 @@ var onQuery = function(){
 		$('#passport-container .passport-container_queryString')[0].innerHTML = $form.val() + 'を過去に検索したのは・・・'
 
 		if(localStorage.getItem(twitteridKey)){
-			appendUser(localStorage.getItem(twitteridKey));
+			setContent(localStorage.getItem(twitteridKey));
 		} else {
 			OAuth.popup('twitter', {cache: true}).then(function(result) {
 				return result.get('/1.1/account/verify_credentials.json');
 			}).then(function(data){
-				appendUser(data.screen_name);
+				setContent(data.screen_name);
 				localStorage.setItem(twitteridKey, data.screen_name);
 			});
 		}
